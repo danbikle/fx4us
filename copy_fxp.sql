b19dce479903179fdb4093be0323c4440e8cd01b
Dan Bikle <dan.bikle@gmail.com>
2015-04-24 22:11:15 +0000

    mo

--- copy_fxp.sql
+++ copy_fxp.sql
@@ -13,8 +13,11 @@ UPDATE fxp SET pair = 'aud_usd' WHERE pair = 'AUD/USD';
 UPDATE fxp SET pair = 'eur_usd' WHERE pair = 'EUR/USD';
 UPDATE fxp SET pair = 'gbp_usd' WHERE pair = 'GBP/USD';
 UPDATE fxp SET pair = 'nzd_usd' WHERE pair = 'NZD/USD';
+UPDATE fxp SET pair = 'usd_cad' WHERE pair = 'USD/CAD';
+UPDATE fxp SET pair = 'usd_jpy' WHERE pair = 'USD/JPY';
+UPDATE fxp SET pair = 'eur_gbp' WHERE pair = 'EUR/GBP';
 
-COPY(SELECT pair,ttime,bid ORDER BY pair,ttime)TO '/csv/fxp.csv' csv header;
+COPY(SELECT pair,ttime,bid FROM fxp ORDER BY pair,ttime)TO '/tmp/fxp.csv' csv header;
 
 -- done
 
